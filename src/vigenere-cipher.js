88655fed544bafa7e670d90a38768da25ac03cde
NkvXness <leonakavexmusic@gmail.com>
2020-10-16 23:05:04 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,55 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+	constructor(machineMode){
+	    this.machineMode = machineMode;
+	    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	}
+
+	encrypt(message, key) {
+		if (!message || !key) throw new Error();
+		message = this.machineMode === false ? message.split('').reverse().join('') : message;
+		message = message.toUpperCase();
+		key = key.toUpperCase();
+		let encryptText = "";
+		let idxKey = 0;
+
+		for(let idxInput = 0; idxInput <= message.length - 1; idxInput++){
+			let startInput = this.alphabet.indexOf(message[idxInput]);
+			if(startInput === -1){
+				encryptText += message[idxInput];
+			}else{
+				let character = (startInput + this.alphabet.indexOf(key[idxKey])) % this.alphabet.length;
+				encryptText += this.alphabet[character];
+				if(idxKey === key.length - 1) idxKey = 0;
+				else idxKey += 1;
+			}	
+		}
+		return encryptText;
+	}
+
+	decrypt(message, key) {
+		if (!message || !key) throw new Error();
+		message = this.machineMode === false ? message.split('').reverse().join('') : message;
+		message = message.toUpperCase();
+		key = key.toUpperCase();
+		let decryptText = "";
+		let idxKey = 0;
+
+		for(let idxInput = 0; idxInput <= message.length - 1; idxInput++){
+			let startInput = this.alphabet.indexOf(message[idxInput]);
+			if(startInput === -1){
+				decryptText += message[idxInput];
+			}else{
+				let character = (startInput + this.alphabet.length - this.alphabet.indexOf(key[idxKey])) % this.alphabet.length;
+				decryptText += this.alphabet[character];
+				if(idxKey === key.length - 1) idxKey = 0;
+				else idxKey += 1;				
+			}			
+		}
+		return decryptText;
+	}
 }
 
 module.exports = VigenereCipheringMachine;
